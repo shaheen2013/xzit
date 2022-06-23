c12ff9130f7719211bc64e99c2079d699c8aefe3
shaheen-sahadat <coredeveloper.2013@gmail.com>
2022-06-23 19:05:03 +0600

    File Manager swagger add browser button

--- filemanager/viewsets.py
+++ filemanager/viewsets.py
@@ -1,7 +1,13 @@
 from rest_framework import viewsets
+from rest_framework.parsers import MultiPartParser
+from rest_framework.decorators import action
 from filemanager.models import FileManager
 from filemanager.serializers import FileManagerSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 class FileManagerViewSet(viewsets.ModelViewSet):
        queryset = FileManager.objects.all()
        serializer_class = FileManagerSerializer
+       parser_classes = (MultiPartParser, )
+       permission_classes = [IsAuthenticated]
