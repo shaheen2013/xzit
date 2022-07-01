437b1dea543fef8fd0cced68d3e47bf84a6db88d
kmrifat <kmrifat@gmail.com>
2022-07-01 19:57:25 +0600

    user password filed now converting the password to hash from the admin panel as well

--- activity/admin.py
+++ activity/admin.py
@@ -1,23 +1,30 @@
 from ast import arg
 from django.contrib import admin
 
-from activity.models import Post, PostComment, PostLike
+from activity.models import Post, PostComment, PostLike, Story
+
 
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-       list_display = ['created_by', 'created_at', 'likes']
-       
-       
-       def likes(self, *args, **kwargs):
-              post_id = args[0].id
-              post_likes = PostLike.objects.filter(post_id=post_id).count()
-              return post_likes
-              
+    list_display = ['created_by', 'created_at', 'likes']
+
+    def likes(self, *args, **kwargs):
+        post_id = args[0].id
+        post_likes = PostLike.objects.filter(post_id=post_id).count()
+        return post_likes
+
+
 @admin.register(PostLike)
 class PostLikeAdmin(admin.ModelAdmin):
-       list_display = ['created_by', 'created_at']
+    list_display = ['created_by', 'created_at']
+
 
 @admin.register(PostComment)
 class PostCommentAdmin(admin.ModelAdmin):
-       list_display = ['post', 'comment']
+    list_display = ['post', 'comment']
+
+
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ['story_type', 'media']
--- authentication/admin.py
+++ authentication/admin.py
@@ -1,11 +1,22 @@
 from re import A
 from django.contrib import admin
+from django import forms
+from django.contrib.auth import hashers
 
 from authentication.models import User
 
 # Register your models here.
+from xzit import settings
+
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-       list_display = ['name' ,'username', 'email', 'created_at']
-       
-       
+    list_display = ['username', 'name', 'email', 'created_at']
+
+    def save_model(self, request, obj, form, change):
+        try:
+            print(hashers.identify_hasher(request.POST['password']))
+            super(UserAdmin, self).save_model(request, obj, form, change)
+        except:
+            obj.password = hashers.make_password(request.POST['password'])
+            super(UserAdmin, self).save_model(request, obj, form, change)
