e5d65118cf1728a40709648a55309334a35e6c9d
shaheen-sahadat <coredeveloper.2013@gmail.com>
2022-07-04 18:40:36 +0600

    Customize admin panel create user

--- authentication/admin.py
+++ authentication/admin.py
@@ -2,6 +2,7 @@ from re import A
 from django.contrib import admin
 from django import forms
 from django.contrib.auth import hashers
+from django.contrib.auth.admin import UserAdmin
 
 from authentication.models import User
 
@@ -10,13 +11,37 @@ from xzit import settings
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(UserAdmin):
     list_display = ['username', 'name', 'email', 'created_at']
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'name', 'password')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email', 'phone', 'profile_image', 'cover_image', 'gender', 'birth_date', 'country_code', 'country', 'city', 'address', 'latitude')
+        }),
+        ('Business info', {
+            'fields': ('business_name', 'business_manager', 'business_type', 'business_days', 'amenties')
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser',
+                'groups', 'user_permissions'
+            )
+        }),
+        ('Important dates', {
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'fields': ('name', 'username', 'email', 'phone', 'password1', 'password2')
+        }),
+    )
 
-    def save_model(self, request, obj, form, change):
-        try:
-            print(hashers.identify_hasher(request.POST['password']))
-            super(UserAdmin, self).save_model(request, obj, form, change)
-        except:
-            obj.password = hashers.make_password(request.POST['password'])
-            super(UserAdmin, self).save_model(request, obj, form, change)
+    # def save_model(self, request, obj, form, change):
+    #     try:
+    #         super(UserAdmin, self).save_model(request, obj, form, change)
+    #     except:
+    #         obj.password = hashers.make_password(request.POST['password'])
+    #         super(UserAdmin, self).save_model(request, obj, form, change)
