4e3f3e2b40fc2bf3975449cebbb23cb4701d8ed3
shaheen-sahadat <coredeveloper.2013@gmail.com>
2022-07-29 13:14:09 +0600

    Cors header solution.

--- xzit/settings.py
+++ xzit/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'commerce',
 
     # Third party app
+    'corsheaders',
     'django_extensions',
     'rest_framework',
     'rest_framework_simplejwt',
@@ -84,6 +85,7 @@ SWAGGER_SETTINGS = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
