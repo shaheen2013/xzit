127f7f22bdd6ac50eb861e7c65d48e5b04d02971
shaheen-sahadat <coredeveloper.2013@gmail.com>
2022-08-02 18:40:06 +0600

    default otp

--- xzit/emails.py
+++ xzit/emails.py
@@ -5,7 +5,7 @@ from django.conf import settings
 def send_otp(email, user):
        subject = 'Your account verification email'
        otp = random.randint(100000, 999999)
-       user.otp = 000000
+       user.otp = "000000"
        user.is_verified = False
        user.save()
        message = f'Your otp is {otp}'
