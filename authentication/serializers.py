5c702efa1e1bf7e08eaac3f242b32a1b93443e3a
shaheen-sahadat <coredeveloper.2013@gmail.com>
2022-08-03 15:07:26 +0600

    user return id

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -8,10 +8,11 @@ from xzit.emails import send_otp
 class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username', 'email', 'phone', 'password', 'tokens', 'otp')
+        fields = ( 'id', 'first_name', 'last_name', 'username', 'email', 'phone', 'password', 'tokens', 'otp')
         extra_kwargs = {
                 'password' : { 'write_only' : True},
-                'otp':  { 'read_only' : True}
+                'otp':  { 'read_only' : True},
+                'id' : { 'read_only' : True }
             }
         
     def create(self, validated_data):
@@ -37,10 +38,11 @@ class UserBasicInfoUpdateSerializer(serializers.ModelSerializer):
 class MerchantRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username', 'email', 'phone', 'password', 'tokens', 'otp')
+        fields = ('id', 'first_name', 'last_name', 'username', 'email', 'phone', 'password', 'tokens', 'otp')
         extra_kwargs = {
                 'password' : { 'write_only' : True},
-                'otp' : { 'read_only' : True}
+                'otp' : { 'read_only' : True},
+                'id' : { 'read_only' : True},
             }
         
     def create(self, validated_data):
