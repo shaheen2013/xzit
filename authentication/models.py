6cb9a0271672c2939d308aaf11f633c5d275fd06
shaheen-sahadat <coredeveloper.2013@gmail.com>
2022-08-11 16:56:15 +0600

    Send reset passwrod token

--- authentication/models.py
+++ authentication/models.py
@@ -109,38 +109,22 @@ class UserSocial(models.Model):
 # class UserBusiness(models.Model):
 #     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
 from django.dispatch import receiver
 from django.urls import reverse
 from django_rest_passwordreset.signals import reset_password_token_created
+from django.core.mail import send_mail
 
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
-    context = {
-        'current_user': reset_password_token.user,
-        'username': reset_password_token.user.username,
-        'email': reset_password_token.user.email,
-        'reset_password_url': "{}?token={}".format(
-            instance.request.build_absolute_uri(reverse('password_reset:reset-password-confirm')),
-            reset_password_token.key)
-    }
-    print(context)
+    email_plaintext_message = f"Your password reset token is: {reset_password_token.key}"
 
-    # render email text
-    email_html_message = render_to_string('email/user_reset_password.html', context)
-    # email_plaintext_message = render_to_string('email/user_reset_password.txt', context)
-
-    msg = EmailMultiAlternatives(
+    send_mail(
         # title:
-        "Password Reset for {title}".format(title="Website Name"),
+        "Password Reset for {title}".format(title="XZIT"),
         # message:
-        context,
+        email_plaintext_message,
         # from:
         "noreply@somehost.local",
         # to:
         [reset_password_token.user.email]
     )
-    msg.attach_alternative(email_html_message, "text/html")
-    msg.send()
-
