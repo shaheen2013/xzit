8b9961763ae938082947da0c3e94535bdeddecfb
shaheen-sahadat <coredeveloper.2013@gmail.com>
2022-08-16 21:21:01 +0600

    login with google fixed

--- social_auth/register.py
+++ social_auth/register.py
@@ -5,6 +5,8 @@ import os
 import random
 from rest_framework.exceptions import AuthenticationFailed
 
+from xzit.emails import send_otp
+
 
 def generate_username(name):
 
@@ -27,6 +29,7 @@ def register_social_user(provider, email, name):
                 username=filtered_user_by_email[0].username, password=os.environ.get('SOCIAL_SECRET'))
 
             return {
+                'id': registered_user.id,
                 'username': registered_user.username,
                 'email': registered_user.email,
                 'tokens': registered_user.tokens()
@@ -38,18 +41,22 @@ def register_social_user(provider, email, name):
 
     else:
         user = {
-            'name' : name,
+            'first_name' : name,
             'username': generate_username(name), 'email': email,
             'password': os.environ.get('SOCIAL_SECRET')}
         
         user = User.objects.create_user(**user)
-        # user.is_verified = True
+        user.is_verified = False
         user.auth_provider = provider
         user.save()
+         # OTP Send
+        if user.email is not None:
+            send_otp(user.email, user)
         
         new_user = authenticate(
             username=user.username, password=os.environ.get('SOCIAL_SECRET'))
         return {
+            'id': new_user.id,
             'email': new_user.email,
             'username': new_user.username,
             'tokens': new_user.tokens()
