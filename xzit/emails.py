2540274fb59c9661330a448b47ffc8566ab8aa64
shaheen-sahadat <coredeveloper.2013@gmail.com>
2022-08-29 22:02:26 +0600

    email template integerate

--- xzit/emails.py
+++ xzit/emails.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
 import random
 from django.conf import settings
+from django.template.loader import render_to_string    
 
 def send_otp(email, user):
        subject = 'Your account verification email'
@@ -8,18 +9,16 @@ def send_otp(email, user):
        user.otp = otp
        user.is_verified = False
        user.save()
-       message = f'Your otp is {otp}'
+       message_html = render_to_string('email/otp_email.html', {'otp': otp})
        email_from = settings.EMAIL_HOST_USER 
-       send_mail(subject, message, email_from, [email])
+       send_mail(subject, message='', from_email=email_from, recipient_list=[email], html_message=message_html)
        
-
 def send_reset_otp(email, user):
        subject = 'Your account verification email'
        otp = random.randint(100000, 999999)
        user.reset_pass_otp = otp
        user.save()
-       print(user)
-       message = f'Your otp is {otp}'
+       message = render_to_string('email/otp_email.html', {'otp': otp})
        email_from = settings.EMAIL_HOST_USER 
        send_mail(subject, message, email_from, [email])
        
