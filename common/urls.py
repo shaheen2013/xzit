4c838bb72f14cf3eadfb2521893b3d0e52466cd0
rakibulislam8226 <rakibulislam8226@gmail.com>
2022-09-28 15:35:22 +0600

    friends add block update

--- common/urls.py
+++ common/urls.py
@@ -5,6 +5,9 @@ from common import views
 urlpatterns = [
     path('reason-list/', views.ReportReasonList.as_view(), name='report-reason'),
     
-    path('friend-list-func/', views.friend_list, name='friend-list-func'),
-    path('friend-block/', views.block_list, name='friend-block'),
+    # path('friend-list-func/', views.friend_list, name='friend-list-func'),
+    path('friend-list/', views.friend_list, name='friend-list'),
+    path('friend-add/', views.friend_add, name='friend-add'),
+    path('add-block/', views.add_block, name='add-block'),
+    path('block-list/', views.block_list, name='block-list'),
 ]
--- common/views.py
+++ common/views.py
@@ -1,7 +1,6 @@
 
 from django.shortcuts import render
 from rest_framework import generics
-
 from commerce.models import User
 from . import serializers
 from rest_framework.permissions import IsAuthenticated
@@ -15,42 +14,44 @@ class ReportReasonList(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
     
     
-@api_view(['POST', 'GET'])  
+@api_view(['POST'])  
+def friend_add(request):
+    data = request.data
+    friend_exist = Friends.objects.filter(user=data['user']).first()
+    block_exist = BlockFriends.objects.filter(user=data['user']).first()
+    if block_exist:
+        return Response("You Can't Add friend to a blocked person.")
+    if friend_exist:
+        return Response(serializers.FriendSerializers(friend_exist).data)
+    serializer=serializers.FriendSerializers(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['GET'])  
 def friend_list(request):
-    if request.method == "GET":
-        serializer = serializers.FriendSerializers(Friends.objects.all(), many=True)
-        return Response(serializer.data)
-    elif request.method=="POST":
-        data = request.data
-        friend_exist = Friends.objects.filter(user=data['user']).first()
-        block_exist = BlockFriends.objects.filter(user=data['user']).first()
-        if block_exist:
-            return Response("You Can't Add friend to a blocked person.")
-        if friend_exist:
-            return Response(serializers.FriendSerializers(friend_exist).data)
-        serializer=serializers.FriendSerializers(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-        return Response(serializer.data)
-    
-@api_view(['POST', 'GET'])  
+    # if request.method == "GET":
+    serializer = serializers.FriendSerializers(Friends.objects.all(), many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])  
 def block_list(request):
-    if request.method == "GET":
-        serializer = serializers.BlockFriendSerializers(BlockFriends.objects.all(), many=True)
-        return Response(serializer.data)
-    elif request.method=="POST":
-        data = request.data
-        friend_exist = Friends.objects.filter(user=data['user']).first()
-        block_exist = BlockFriends.objects.filter(user=data['user']).first()
-        if block_exist:
-            return Response(serializers.BlockFriendSerializers(friend_exist).data)
-        if friend_exist:
-            friend_exist.delete()
-            
-        serializer=serializers.BlockFriendSerializers(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-        return Response(serializer.data)
+    serializer = serializers.BlockFriendSerializers(BlockFriends.objects.all(), many=True)
+    return Response(serializer.data)
+    
+@api_view(['POST'])  
+def add_block(request):
+    data = request.data
+    friend_exist = Friends.objects.filter(user=data['user']).first()
+    block_exist = BlockFriends.objects.filter(user=data['user']).first()
+    if block_exist:
+        return Response(serializers.BlockFriendSerializers(friend_exist).data)
+    if friend_exist:
+        friend_exist.delete()
+    serializer=serializers.BlockFriendSerializers(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+    return Response(serializer.data)
 
     
         
